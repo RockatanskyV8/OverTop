f6ecb0ba0b1329943165b677c2b29088d6815a02
Lucas Santos Rosa <luccasanttoz@gmail.com>
2018-10-29 21:45:51 -0300

    simplificando um pouco e hitdetection

--- interface.py
+++ interface.py
@@ -34,9 +34,7 @@ class interface:
                          [[200, 350],[200, 400]] ]
 
         self.draw_square(self.c.vertices)
-        #print(self.paredes)
         self.create_parede(self.paredes, fill='red', tag='parede')
-        #self.canvas.create_line([  0, 150],[400, 150], fill='red', tag='parede')
 
         self._animate()
         self._set_bindings()
@@ -45,39 +43,15 @@ class interface:
         self.canvas.pack()
         mainloop()
 
-################################################################################
+################################################################################ DESENHAR
 
     def draw_square(self, points, color="blue", nome = "player"):
         self.canvas.create_polygon(points, fill=color, tag=nome)
         self.canvas.delete('bola')
         self.exibirCenter(points)
-        #print(self.c.contorno)
-        self.contorno()
-        self.hit_detection()
         self.exibirDirecoes()
-
-    def create_circle(self, x, y, r, **kwargs):
-        self.canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
-
-    def create_parede(self, parede, **kwargs):
-        for p in parede:
-            self.canvas.create_line(p, **kwargs)
-
-    def contorno(self):
-        self.c.referencia_linhas()
-        for lin in self.c.contorno:
-            self.canvas.create_line(lin, fill="white", tag="player")
-
-    def hit_detection(self):
-        for p in self.paredes:
-            self.hit(p)
-
-    def hit(self, parede):
-        for lin in self.c.contorno:
-            col = self.c.exists_intersection(lin, parede)
-            if col != []:
-                x, y = col
-                self.create_circle(x, y, 5, fill="yellow",  width=1, tag='bola')
+        c = COL_HANDLER(points, self.paredes)
+        self.hit(c.hit_detection())
 
     def exibirCenter(self, vertices):
         coords = vertices
@@ -89,30 +63,44 @@ class interface:
         self.canvas.create_line(self.c.centroid() , coords[0], fill="yellow", tag="player")
         self.canvas.create_line(self.c.centroid() , coords[1], fill="red"   , tag="player")
 
-    def rotacionar(self, angle = 1, nome="player", cor="blue"):
+    def create_parede(self, parede, **kwargs):
+        for p in parede:
+            self.canvas.create_line(p, **kwargs)
+
+################################################################################ MOVIMENTACAO
+
+    def rotacionar(self, angle = 1, nome="player"):
         self.c.rotacao(angle)
         self.canvas.delete(nome)
-        self.draw_square(self.c.vertices , cor)
+        self.draw_square(self.c.vertices)
 
-    def movFrente(self, vel = 1, nome="player", cor="blue"):
-        x , y = self.c.orientacao(0)
+    def mover(self, orientacao, vel = 1, nome = "player"):
+        x , y = orientacao
         self.c.mover( x * vel, y * vel )
         self.canvas.delete(nome)
-        self.draw_square(self.c.vertices , cor)
+        self.draw_square(self.c.vertices)
 
-    def movLado(self, vel = 1, nome="player", cor="blue"):
-        x, y = self.c.orientacao(1)
-        self.c.mover( x * vel, y * vel )
-        self.canvas.delete(nome)
-        self.draw_square(self.c.vertices , cor)
+
+################################################################################ COLISAO
+
+    def create_circle(self, x, y, r, **kwargs):
+        self.canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+
+    def hit(self, vect):
+        if vect != []:
+            for v in vect:
+                x , y = v
+                self.create_circle(x, y, 5, fill="yellow", width=1, tag='bola')
+
+################################################################################ CONTROLES
 
     def _animate(self):
-        if self.controles["w"]: self.movFrente(1)
+        if self.controles["w"]: self.mover(self.c.orientacao(0))
         if self.controles["a"]: self.rotacionar(-1)
-        if self.controles["s"]: self.movFrente(-1)
+        if self.controles["s"]: self.mover(self.c.orientacao(0), -1)
         if self.controles["d"]: self.rotacionar()
-        if self.controles["o"]: self.movLado()
-        if self.controles["k"]: self.movLado(-1)
+        if self.controles["o"]: self.mover(self.c.orientacao(1))
+        if self.controles["k"]: self.mover(self.c.orientacao(1), -1)
 
         self.root.after(30, self._animate)
 
